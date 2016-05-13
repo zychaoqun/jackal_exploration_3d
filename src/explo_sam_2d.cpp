6bb67807e6113588934b2d83efb4328ab3f2326e
bona <sbai1@stevens.edu>
2016-05-13 10:00:05 -0400

    sync between 2d and 3d map, using hector mapper as well

--- src/explo_sam_2d.cpp
+++ src/explo_sam_2d.cpp
@@ -31,7 +31,7 @@ typedef pcl::PointXYZ PointType;
 typedef pcl::PointCloud<pcl::PointXYZ> PointCloud;
 const double PI = 3.1415926;
 const double free_prob = 0.3;
-const double z_sensor = 0.2972;
+// const double z_sensor = 0.2972;
 // octomap::OcTree *tree;
 octomap::OcTree* cur_tree;
 bool octomap_flag = 0; // 0 : msg not received
@@ -64,14 +64,15 @@ struct SensorModel {
             : width(_width), height(_height), horizontal_fov(_horizontal_fov), vertical_fov(_vertical_fov), max_range(_max_range) {
         angle_inc_hor = horizontal_fov / width;
         angle_inc_vel = vertical_fov / height;
-        for(double i = -width / 2; i < width / 2; ++i) {
-                pitch_yaws.push_back(make_pair(0.0, i * angle_inc_hor));
-            }
+        for(double j = -height / 2; j < height / 2; ++j) 
+            for(double i = -width / 2; i < width / 2; ++i) {
+                pitch_yaws.push_back(make_pair(j * angle_inc_vel, i * angle_inc_hor));
+        }
     }
 }; 
 
 // SensorModel Velodyne_puck(3600, 16, 2*PI, 2/9*PI, 100.0 );
-SensorModel Velodyne_puck(3600, 3, 2*PI, 0.1, 100.0 );
+SensorModel Velodyne_puck(3600, 16, 2*PI, 0.523, 100.0 );
 
 
 double get_free_volume(const octomap::OcTree *octree) {
@@ -123,13 +124,13 @@ vector<point3d> cast_sensor_rays(const octomap::OcTree *octree, const point3d &p
 }
 
 vector<pair<point3d, point3d>> generate_candidates(point3d position) {
-    double R = 1;   // Robot step, in meters.
+    double R = 1.0;   // Robot step, in meters.
     double n = 5;
     int counter = 0;
     octomap::OcTreeNode *n_cur;
 
     vector<pair<point3d, point3d>> candidates;
-    double z = z_sensor;                // fixed 
+    double z = position.z();                // fixed 
     double pitch = 0;                   // fixed
     double x, y;
 
@@ -144,7 +145,7 @@ vector<pair<point3d, point3d>> generate_candidates(point3d position) {
             counter++;
         }
 
-    cout << "Generate Candidates : " << counter << endl;
+    cout << "Candidates : " << counter << endl;
     return candidates;
 }
 
@@ -194,7 +195,7 @@ void velodyne_callbacks( const sensor_msgs::PointCloud2ConstPtr& cloud2_msg ) {
     for (int j = 1; j< cloud->width; j++)
     {
         if(isnan(cloud->at(j).x)) continue;
-        cur_tree->insertRay(point3d( position.x(),position.y(),z_sensor), 
+        cur_tree->insertRay(point3d( position.x(),position.y(),position.z()), 
             point3d(cloud->at(j).x, cloud->at(j).y, cloud->at(j).z), Velodyne_puck.max_range);
         // add the point into map
         map_pcl->points.push_back(pcl::PointXYZ(cloud_local->at(j).x, cloud_local->at(j).y, cloud_local->at(j).z));
@@ -202,7 +203,8 @@ void velodyne_callbacks( const sensor_msgs::PointCloud2ConstPtr& cloud2_msg ) {
     }
     map_pcl->height = 1;
     
-    cout << "height : " << cloud->height << "  , width :" << cloud->width << " resulting map free volume  " << get_free_volume(cur_tree) << endl;
+    // cout << "height : " << cloud->height << "  , width :" << cloud->width << " resulting map free volume  " << get_free_volume(cur_tree) << endl;
+    cout << "Entropy(velo) : " << get_free_volume(cur_tree) << endl;
     // delete octree_copy;
     map_pcl->header.stamp = ros::Time::now().toNSec() / 1e3;
     Map_pcl_pub.publish(map_pcl);
@@ -226,10 +228,11 @@ void hokuyo_callbacks( const sensor_msgs::PointCloud2ConstPtr& cloud2_msg )
     for (int j = 1; j< cloud->width; j++)
     {
         if(isnan(cloud->at(j).x)) continue;
-        cur_tree->insertRay(point3d( position.x(),position.y(),z_sensor), 
+        cur_tree->insertRay(point3d( position.x(),position.y(),position.z()), 
             point3d(cloud->at(j).x, cloud->at(j).y, cloud->at(j).z), Velodyne_puck.max_range);
     }
-    cout << "Map  Entropy after laser scan : " << get_free_volume(cur_tree) << endl;
+    cout << "Entropy(scan) : " << get_free_volume(cur_tree) << endl;
+    // ROS_INFO("Sending goal");
     delete cloud;
     delete cloud_local;
 
@@ -248,6 +251,9 @@ int main(int argc, char **argv) {
     
     ros::Subscriber hokuyo_sub;
     hokuyo_sub = nh.subscribe<sensor_msgs::PointCloud2>("/hokuyo_points", 1, hokuyo_callbacks);
+
+    ros::Publisher GoalMarker_pub = nh.advertise<visualization_msgs::Marker>( "Goal_Marker", 0 );
+    ros::Publisher JackalMarker_pub = nh.advertise<visualization_msgs::Marker>( "Jackal_Marker", 0 );
 
     tf_listener = new tf::TransformListener();
     tf::StampedTransform transform;
@@ -280,7 +286,7 @@ int main(int argc, char **argv) {
     // point3d dif_pos = point3d(0,0,0);
     point3d eu2dr(1, 0, 0);
     octomap::OcTreeNode *n;
-    octomap::OcTree new_tree(0.2);
+    octomap::OcTree new_tree(0.1);
     cur_tree = &new_tree;
 
     // Get the current localization from tf
@@ -288,7 +294,7 @@ int main(int argc, char **argv) {
     while (!find_tf)
     {
         try{
-        tf_listener->lookupTransform("/map", "/base_link", ros::Time(0), transform);
+        tf_listener->lookupTransform("/map", "/laser", ros::Time(0), transform);
         position = point3d(transform.getOrigin().x(), transform.getOrigin().y(), transform.getOrigin().z());
         find_tf = true;
         }
@@ -304,12 +310,39 @@ int main(int argc, char **argv) {
 
     // Update latest localization
     try{
-        tf_listener->lookupTransform("/map", "/base_link", ros::Time(0), transform);
+        tf_listener->lookupTransform("/map", "/laser", ros::Time(0), transform);
         position = point3d(transform.getOrigin().x(), transform.getOrigin().y(), transform.getOrigin().z());
     }
       catch (tf::TransformException ex){
       ROS_ERROR("%s",ex.what());
     }
+
+    // Visualize current position
+    RPY2Quaternion(0, 0, transform.getRotation().getAngle(), &qx, &qy, &qz, &qw);
+    // Publish the goal as a Marker in rviz
+    visualization_msgs::Marker jackal_frame;
+    jackal_frame.header.frame_id = "map";
+    jackal_frame.header.stamp = ros::Time();
+    jackal_frame.ns = "robot_frame";
+    jackal_frame.id = 0;
+    jackal_frame.type = visualization_msgs::Marker::ARROW;
+    jackal_frame.action = visualization_msgs::Marker::ADD;
+    jackal_frame.pose.position.x = position.x();
+    jackal_frame.pose.position.y = position.y();
+    jackal_frame.pose.position.z = position.z();
+    jackal_frame.pose.orientation.x = qx;
+    jackal_frame.pose.orientation.y = qy;
+    jackal_frame.pose.orientation.z = qz;
+    jackal_frame.pose.orientation.w = qw;
+    jackal_frame.scale.x = 0.5;
+    jackal_frame.scale.y = 0.1;
+    jackal_frame.scale.z = 0.1;
+    jackal_frame.color.a = 1.0; // Don't forget to set the alpha!
+    jackal_frame.color.r = 1.0;
+    jackal_frame.color.g = 0.0;
+    jackal_frame.color.b = 0.0;
+    // JackalMarker_pub.publish( jackal_frame );
+
     // Initial Scan
     ros::spinOnce();
 
@@ -319,7 +352,7 @@ int main(int argc, char **argv) {
         vector<pair<point3d, point3d>> candidates = generate_candidates(position);
         vector<double> MIs(candidates.size());
         double before = get_free_volume(cur_tree);
-        cout << "CurMap  Entropy : " << get_free_volume(cur_tree) << endl;
+        // cout << "CurMap  Entropy : " << get_free_volume(cur_tree) << endl;
         max_idx = 0;
 
         // for every candidate...
@@ -336,29 +369,54 @@ int main(int argc, char **argv) {
             vector<point3d> hits = cast_sensor_rays(cur_tree, c.first, eu2dr);
             MIs[i] = calc_MI(cur_tree, c.first, hits, before);
 
-            cout << "X." << i << ". .";
+            // cout << "X." << i << ". .";
             // Pick the Best Candidate
             if (MIs[i] > MIs[max_idx])
             {
                 max_idx = i;
             }
         }
-        cout << endl;
+        // cout << endl;
         cout << "Max MI : " << MIs[max_idx] << endl;
+
+        RPY2Quaternion(0, 0, candidates[max_idx].second.yaw(), &qx, &qy, &qz, &qw);
+        // Publish the goal as a Marker in rviz
+        visualization_msgs::Marker marker;
+        marker.header.frame_id = "map";
+        marker.header.stamp = ros::Time();
+        marker.ns = "goal_marker";
+        marker.id = 0;
+        marker.type = visualization_msgs::Marker::ARROW;
+        marker.action = visualization_msgs::Marker::ADD;
+        marker.pose.position.x = candidates[max_idx].first.x();
+        marker.pose.position.y = candidates[max_idx].first.y();
+        marker.pose.position.z = candidates[max_idx].first.z();
+        marker.pose.orientation.x = qx;
+        marker.pose.orientation.y = qy;
+        marker.pose.orientation.z = qz;
+        marker.pose.orientation.w = qw;
+        marker.scale.x = 0.5;
+        marker.scale.y = 0.1;
+        marker.scale.z = 0.1;
+        marker.color.a = 1.0; // Don't forget to set the alpha!
+        marker.color.r = 1.0;
+        marker.color.g = 1.0;
+        marker.color.b = 0.0;
+        GoalMarker_pub.publish( marker );
+
+
         // Send the Robot 
         cout << "Sending the Goal : " << candidates[max_idx].first << " , Yaw : " << candidates[max_idx].second.yaw() << endl;
-        RPY2Quaternion(0, 0, candidates[max_idx].second.yaw(), &qx, &qy, &qz, &qw);
-
+        
         bool arrived = goToDest(candidates[max_idx].first, qx, qy, qz, qw);
-
 
         // cout << "Current Robot Location : " << position << endl;
         if(arrived)
         {
-            ROS_INFO("We made one step forward");
+            ROS_INFO("Forward");
             // Update latest localization
             try{
-                tf_listener->lookupTransform("/map", "/base_link", ros::Time(0), transform);
+                tf_listener->lookupTransform("/map", "/laser", ros::Time(0), transform);
                 position = point3d(transform.getOrigin().x(), transform.getOrigin().y(), transform.getOrigin().z());
             }
               catch (tf::TransformException ex){
@@ -369,9 +427,9 @@ int main(int argc, char **argv) {
         }
         else
         {
-            cout << "****************    We didn't make it... Check your code..." << endl;
+            cout << "move_base failed..." << endl;
         }
-        cout << "CurMap  Entropy after movement : " << get_free_volume(cur_tree) << endl;
+        // cout << "CurMap  Entropy after movement : " << get_free_volume(cur_tree) << endl;
         // r.sleep();
     }
     nh.shutdown();          
